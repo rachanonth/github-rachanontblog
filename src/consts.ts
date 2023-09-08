56acbe465a02dd2df24bab144d9f225e5f83b416
rachanonth <42505598+rachanonth@users.noreply.github.com>
2023-09-08 10:38:15 +0700

    1 binaries uploaded

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Rachanont';
+export const SITE_DESCRIPTION = 'On my blog, I dive into everything from pharmacy insights to the latest in tech know-how, and I even sprinkle in some financial wisdom for good measure.';
